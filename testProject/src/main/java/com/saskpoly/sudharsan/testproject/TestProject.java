749826ee3adb24df199a5dd4d37b3105c298ad25
Sudharsan-Murali <sudharsan.murali@proton.me>
2025-01-25 12:46:22 -0600

    test project created and run successfully without any errors on college machine

---
+++ testProject/src/main/java/com/saskpoly/sudharsan/testproject/TestProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.saskpoly.sudharsan.testproject;
+
+/**
+ *
+ * @author Student
+ */
+public class TestProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+        System.out.println("Hello Sudharsan!");
+    }
+}
